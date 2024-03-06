2dc6a11fb32104fcf983dff4a6c9eefd22153b74
Viktoria Bratskih <peace.kokos@gmail.com>
2024-03-06 14:13:28 +0600

    sport components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import Sport from "./components/sportComponents/Sport";
+import OnlineCinema from "./components/OnlineCinema/OnlineCinema";
 
 const App = () => {
   return (
     <div>
-      <Sport />
+      <OnlineCinema />
       <MainRoutes />
     </div>
   );
