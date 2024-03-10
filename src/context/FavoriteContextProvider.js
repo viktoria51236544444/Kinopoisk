13d90c93633c5ba6841489ec354df4a42fcd9777
Your Name <you@example.com>
2024-03-10 18:27:06 +0600

    ff

--- src/context/FavoriteContextProvider.js
+++ src/context/FavoriteContextProvider.js
@@ -62,15 +62,3 @@ const FavoriteContextProvider = ({ children }) => {
 };
 
 export default FavoriteContextProvider;
-
-import React, { createContext, useContext } from "react";
-const favoriteMovie = createContext();
-export const useFavorite = () => useContext(favoriteMovie);
-const FavoriteContextProvider = ({ children }) => {
-  const values = {};
-  return (
-    <favoriteMovie.Provider value={values}>{children}</favoriteMovie.Provider>
-  );
-};
-
-export default FavoriteContextProvider;
