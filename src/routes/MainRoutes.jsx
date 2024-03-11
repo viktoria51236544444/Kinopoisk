64d6f5b1ad813b0e02feb9c5e6c36a8e4ab55822
Your Name <you@example.com>
2024-03-11 14:13:56 +0600

    s

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import OscarPage from "../pages/OscarPage";
 import OscarGamePage from "../pages/OscarGamePage";
 import OscarNomineesPage from "../pages/OscarNomineesPage";
-import KidsNavbar from "../components/kids/KidsNavbar";
+
 import KidsPage from "../pages/KidsPage";
 import Dog from "../components/kids/Dog";
 import Anime from "../components/kids/Anime";
@@ -14,17 +14,18 @@ import LoginForm from "../auth_redux/componentsAuth/loginForm/LoginForm";
 import RegistrationForm from "../auth_redux/componentsAuth/registration/RegistrationForm";
 import PageAdd from "../pages/PageAdd";
 import EditProduct from "../components/product/EditProduct";
-import MainPage from "../pages/MainPage";
+
 import Sport from "../components/sportComponents/Sport";
 import OnlineCinema from "../components/OnlineCinema/OnlineCinema";
 import MoviesCart from "../components/product/MoviesCart";
 import MoviesList from "../components/product/MoviesList";
 import Favorite from "../components/MoviesAndSeries/Favorite";
 import AddReviews from "../components/MoviesAndSeries/AddReviews";
-import ReviewsCard from "../components/MoviesAndSeries/ReviewsCard";
 import ReviesList from "../components/MoviesAndSeries/ReviesList";
 import EditReviews from "../components/MoviesAndSeries/EditReviews";
 import OscarGameCondition from "../components/oscarGamePage/OscarGameCondition";
+import MainPage from "../pages/MainPage";
+import NomineesGame from "../components/oscarGamePage/NomineesGame";
 
 const MainRoutes = () => {
   return (
@@ -47,11 +48,12 @@ const MainRoutes = () => {
       <Route path="/onlineCinema" element={<OnlineCinema />} />
       <Route path="/moviesCard" element={<MoviesCart />} />
       <Route path="/moviesList" element={<MoviesList />} />
-      {/* <Route path="/gameCondition" element={<OscarGameCondition />} /> */}
+      <Route path="/gameCondition" element={<OscarGameCondition />} />
       <Route path="/favorite" element={<Favorite />} />
       <Route path="/addReviews" element={<AddReviews />} />
       <Route path="/rewiesList" element={<ReviesList />} />
       <Route path="/editReviews/:id" element={<EditReviews />} />
+      <Route path="/game" element={<NomineesGame />} />
     </Routes>
   );
 };
